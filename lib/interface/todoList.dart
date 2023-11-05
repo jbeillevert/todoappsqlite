d83eee361c11c43ea48772dad72af098be5b4796
J-E <beillevertj.contact@gmail.com>
2023-11-05 12:15:06 +0100

    tp terminé

--- lib/interface/todoList.dart
+++ lib/interface/todoList.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import '../databaseHelper.dart';
+import '../models/todo_model.dart';
 
 class TodoListDisplay extends StatefulWidget {
-  const TodoListDisplay({super.key});
+  final List<Todo> todos; 
+  final VoidCallback onTodoToggle; 
+
+  const TodoListDisplay({
+    Key? key,
+    required this.todos, 
+    required this.onTodoToggle, 
+  }) : super(key: key);
 
   @override
   State<TodoListDisplay> createState() => _TodoListDisplayState();
@@ -10,8 +19,33 @@ class TodoListDisplay extends StatefulWidget {
 class _TodoListDisplayState extends State<TodoListDisplay> {
   @override
   Widget build(BuildContext context) {
-    return const Column(
-
+    
+    return ListView.builder(
+      itemCount: widget.todos.length,
+      itemBuilder: (context, index) {
+        final todo = widget.todos[index];
+        return ListTile(
+          title: Text(todo.task),
+          leading: Checkbox(
+            value: todo.isCompleted,
+            onChanged: (bool? newValue) {
+              if (newValue != null) {
+                DatabaseHelper.instance.update(todo.copyWith(isCompleted: newValue)).then((_) {
+                  widget.onTodoToggle(); 
+                });
+              }
+            },
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              DatabaseHelper.instance.delete(todo.id!).then((_) {
+                widget.onTodoToggle(); 
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -1,30 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app_sqlite_freezed/interface/todoList.dart';
-import './interface/bottomSheet.dart';
+import 'databaseHelper.dart';
+import 'models/todo_model.dart';
+import 'interface/todoList.dart';
+import 'interface/bottomSheet.dart';
 
-class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  late Future<List<Todo>> _todoListFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _todoListFuture = DatabaseHelper.instance.getAllTodos();
+  }
+
+  void _addTodo(String taskName) async {
+    final newTodo = Todo(
+      task: taskName,
+      isCompleted: false,
+    );
+    await DatabaseHelper.instance.insert(newTodo);
+    _refreshTodoList();
+  }
+
+  void _refreshTodoList() {
+    setState(() {
+      _todoListFuture = DatabaseHelper.instance.getAllTodos();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("TP todo App sqlite"),
-          centerTitle: true,
-          titleTextStyle: const TextStyle(fontSize: 16, color: Colors.white),
-          backgroundColor: Colors.black54,
-        ),
-        body: const Center(
-          child: TodoListDisplay(),
-        ),
-        floatingActionButton: FloatingActionButton.small(
-          onPressed: () async {
-            final newTask = await showModalBottomSheet<String>(
-                context: context, builder: (context) => const BottomSheetPopup()
+      appBar: AppBar(
+        title: const Text("TP todo App SQLite"),
+        centerTitle: true,
+        backgroundColor: Colors.black54,
+      ),
+      body: FutureBuilder<List<Todo>>(
+        future: _todoListFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            return TodoListDisplay(
+              todos: snapshot.data!,
+              onTodoToggle: _refreshTodoList,
             );
-          },
-          child: const Icon(Icons.add),
-        ),
-      );
+          } else {
+            return const Text('No todos found.');
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newTask = await showModalBottomSheet<String>(
+            context: context,
+            builder: (context) => const BottomSheetPopup(),
+          );
+          if (newTask != null) {
+            _addTodo(newTask);
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
